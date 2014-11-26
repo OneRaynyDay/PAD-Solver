9e9e71cd0ed2a15e1d21921565acea7d76182c4a
OldGuo <young.guo@gmail.com>
2014-11-26 10:06:18 -0800

    logic is hard my brain hurts

--- PADSolver/src/BoardFinder.java
+++ PADSolver/src/BoardFinder.java
@@ -22,7 +22,8 @@ public class BoardFinder {
 	private static int RES_DOWNSCALE = 4; //iPhone 5c 640x1136
 	
 	private static BufferedImage img;
-	private static Pixel [][] board;
+	private static Pixel [][] pixelData;
+	private static Color[][]board;
 	
 	public static void main(String [] args) throws IOException{
 		createAndShowGUI();
@@ -36,43 +37,44 @@ public class BoardFinder {
 	}
 	public static void parseImage(){
 		//create pixels 
-		board = new Pixel[height/RES_DOWNSCALE][width/RES_DOWNSCALE]; //arbitrarily only checks every 10 pixels for testing
+		pixelData = new Pixel[height/RES_DOWNSCALE][width/RES_DOWNSCALE]; //arbitrarily only checks every 10 pixels for testing
 		
 		//fills 2D array of pixels
 		for(int i = 0; i < height; i+=RES_DOWNSCALE){
 			for(int j = 0; j < width; j+=RES_DOWNSCALE){
 				Pixel pixel = new Pixel(img.getRGB(j,i));
-				board[i/RES_DOWNSCALE][j/RES_DOWNSCALE] = pixel;
+				pixelData[i/RES_DOWNSCALE][j/RES_DOWNSCALE] = pixel;
 			}
 			System.out.println();
 		}
 		//prints array of pixels
-		for(int k = 0; k < board.length;k++){
-			for(int l = 0; l < board[k].length;l++){
+		for(int k = 0; k < pixelData.length;k++){
+			for(int l = 0; l < pixelData[k].length;l++){
 				//System.out.print(board[k][l]);
 			}
 		//	System.out.println();
 		}
 		showData();
-		fillBoard();
+		showOrbs();
 	}
 	public static void showData(){
 		JFrame frame = new JFrame("What Computer Sees (don't worry about cutoff)");
 		//I believe cutoff has to deal with fitting jpanels in jframes
 		
-		frame.setPreferredSize(new Dimension(width/2,height/2));
+		frame.setPreferredSize(new Dimension(width/2+20,height/2+50));
 		JPanel pane = new JPanel(){
 			protected void paintComponent(Graphics g){
 				g.drawRect(0, 0, getWidth(), getHeight()/RES_DOWNSCALE);
 				//allows for easier visualization of the computer interpretation
-				for(int i = 0; i < board.length;i++){
-					for(int j = 0; j < board[i].length;j++){
-						Color temp = ((Pixel)board[i][j]).getColor();
+				for(int i = 0; i < pixelData.length;i++){
+					for(int j = 0; j < pixelData[i].length;j++){
+						Color temp = ((Pixel)pixelData[i][j]).getColor();
 						g.setColor(temp);
 						g.fillRect(j*RES_DOWNSCALE/2,i*RES_DOWNSCALE/2,RES_DOWNSCALE/2,RES_DOWNSCALE/2);
 					}
 				}
 				showGrid(g);
+				fillBoard();
 			}
 		};
 		frame.add(pane);
@@ -83,11 +85,11 @@ public class BoardFinder {
 		int blockWidth = (width/2)/6;
 		g.setColor(Color.RED);
 		for(int k = 0; k < 6; k++){
-			for(int j = 1; j < 6;j++){
+			for(int l = 1; l < 6;l++){
 				Graphics2D g2 = (Graphics2D) g;
 				//Allows for more visibility in the grid
 			    g2.setStroke(new BasicStroke(5));
-				g.drawRect(k*blockWidth,height/2 - j*blockWidth,blockWidth,blockWidth);
+				g.drawRect(k*blockWidth,height/2 - l*blockWidth,blockWidth,blockWidth);
 			}
 		}
 	}
@@ -99,5 +101,55 @@ public class BoardFinder {
 		 * match that color to an orb type
 		 * then populate the array
 		 * */
+		board = new Color[5][6];
+		int blockWidth = (width/2)/6;
+		//for(int k = 0; k < 5; k++){ //runs through each square in the 6x5 grid
+			int RED = 0;
+			int GREEN = 0;
+			int BLUE = 0;
+			//for(int l = 0; l < 6;l++){
+				for(int i = 0; i < blockWidth;i++){ //runs through every pixel in every square
+					for(int j =	 0; j < blockWidth;j++){
+						System.out.println("("+(blockWidth + i)+","+(blockWidth+j)+")");
+						Pixel temp = pixelData[i][j];
+						//Pixel temp = pixelData[i][j];
+						RED += temp.getRed();
+						GREEN+=temp.getGreen();
+						BLUE+=temp.getBlue();
+					}
+				}
+				RED/=(blockWidth*blockWidth);
+				GREEN/=(blockWidth*blockWidth);
+				BLUE/=(blockWidth*blockWidth);
+			//	board[k][l]=new Color(RED,GREEN,BLUE);
+			//}
+		}
+	
+	public static void showOrbs(){
+		JFrame frame = new JFrame("ORBS");
+		//I believe cutoff has to deal with fitting jpanels in jframes
+		
+		final int blockWidth = (width/2)/6;
+		frame.setPreferredSize(new Dimension(width/2+20,height/2+50));
+		JPanel pane = new JPanel(){
+			protected void paintComponent(Graphics g){
+
+				g.fillRect(0,0,50,50);
+				for(int i = 0; i < 5; i++){
+					for(int j = 0; j < 6;j++){
+						/*if(board[i][j]!= null){
+							g.setColor(board[i][j]);
+							g.fillRect(i*blockWidth, j*blockWidth, blockWidth, blockWidth);
+						}else{
+							System.out.println("null");
+						}*/
+					}
+				}
+			}
+		};
+		frame.add(pane);
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 }
